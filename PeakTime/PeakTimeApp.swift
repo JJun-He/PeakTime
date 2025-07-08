d59c09b90a36a24c6ed16f529bac35d0c1fda689
junhui <junhuilim@kakao.com>
2025-07-09 01:00:20 +0900

    Initial Commit

---
+++ PeakTime/PeakTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeakTimeApp.swift
+//  PeakTime
+//
+//  Created by 임뚱보 on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PeakTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PeakTimeTests/PeakTimeTests.swift
@@ -0,0 +1,16 @@
+//
+//  PeakTimeTests.swift
+//  PeakTimeTests
+//
+//  Created by 임뚱보 on 7/9/25.
+//
+
+import Testing
+
+struct PeakTimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
